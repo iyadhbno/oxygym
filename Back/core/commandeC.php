a71b00fc78399fbc7c90528d4bd4eea64f838913
iyadhbno <58296747+iyadhbno@users.noreply.github.com>
2019-12-05 10:50:30 +0100

    Update commandeC.php

--- Back/core/commandeC.php
+++ Back/core/commandeC.php
@@ -3,120 +3,82 @@ include_once "../config.php";
 
 class commandeC
 {
-	function ajoutercommande($commande)
-	{
-		$sql="insert into commande (idCom,dateCom,prixCom,lieuCom,idClient) values (:idCom,:dateCom,:prixCom,:lieuCom,:idClient)";
-		$db = config::getConnexion();
-		
-		try
-		{
-       
-        $idCom=$commande->get_idCom();
-        $dateCom=$commande->get_dateCom();
-        $prixCom=$commande->get_prixCom();
-        $lieuCom=$commande->get_lieuCom();
-        $idClient=$commande->get_idClient();
-
-        $req=$db->prepare($sql);
-     
-		$req->bindValue(':idCom',$idCom);
-		$req->bindValue(':dateCom',$dateCom);
-		$req->bindValue(':prixCom',$prixCom);
-		$req->bindValue(':lieuCom',$lieuCom);
-		$req->bindValue(':idClient',$idClient);
-        $req->execute();
-           
-        }
-
-        catch (Exception $e)
-        {
-            echo 'Erreur: '.$e->getMessage();
-        }
-
-	}
-/*
-	function recuperercommande($idClient)
-	{
-   		$sql="SELECT * from historiquecommande where idClient=$idClient";
-		$db = config::getConnexion();
-		
-		try
-		{
-		$liste=$db->query($sql);
-		$liste->execute();
-		return $liste;
-		}
-        catch (Exception $e)
-        {
-            die('Erreur: '.$e->getMessage());
-        }
-	}
 	
-	function recuperercommande1($idClient)
-	{
-   		$sql="SELECT * from commande  where idClient=$idClient";
+ 
+}
+
+
+//////////////////////////////////////////////////////
+	
+	function affichercommande(){
+		$sql="SElECT * From commande ORDER BY idcom";
 		$db = config::getConnexion();
-		
-		try
-		{
+		try{
 		$liste=$db->query($sql);
-		$liste->execute();
 		return $liste;
 		}
-        catch (Exception $e)
-        {
+        catch (Exception $e){
             die('Erreur: '.$e->getMessage());
-        }
+        }	
+
 	}
 
-	function recupererdernierID()
-	{
-		$sql="SELECT MAX(idCommande) as max from commande";
-		$db = config::getConnexion();
-		
-		try
-		{
-		$liste=$db->query($sql);
-		$liste->execute();
-		return $liste;
-		}
-        catch (Exception $e)
-        {
-            die('Erreur: '.$e->getMessage());
-        }
-	}
 
-	function recuperercontenucommande($idCommande)
-	{
-        $sql="SELECT * from lignecommande where idCommande=$idCommande";
-		$db = config::getConnexion();
-		
-		try
-		{
-		$liste=$db->query($sql);
-		$liste->execute();
-		return $liste;
-		}
-        catch (Exception $e)
-        {
-            die('Erreur: '.$e->getMessage());
-        }
-	}
+	/////////////////////////////////////////
 
-	function notification($idClient)
-	{
-		$sql="SELECT count(*) as cnt from historiquecommande where idClient=:idClient and etatCommande='Validee' ";
+
+function supprimercommande($idcom){
+		$sql="DELETE FROM commande where idcom= :idcom";
 		$db = config::getConnexion();
         $req=$db->prepare($sql);
-		$req->bindValue(':idClient',$idClient);
+		$req->bindValue(':idcom',$idcom);
 		try{
             $req->execute();
-            return $req;
+           // header('Location: index.php');
         }
-        catch (Exception $e)
-        {
+        catch (Exception $e){
             die('Erreur: '.$e->getMessage());
         }
 	}
-}*/
+
+
+
+	///////////////////////////////////////////
+
+
+	function modifiercommande($commande,$idcom){
+		$sql="UPDATE commande SET idcom=:idcom, datecom=:datecom, prixcom=:prixcom, lieucom=:lieucom, idclient=:idclient, idlivreur=:idlivreur, etatcom=:etatcom WHERE idcom=:idcom";
+		
+		$db = config::getConnexion();
+		//$db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+try{		
+        $req=$db->prepare($sql);
+		$idcom=$commande->getidcom();
+        $datecom=$commande->getdatecom();
+        $prixcom=$commande->getprixcom();
+        $lieucom=$commande->getlieu();
+        $idclient=$commande->getidclient();
+        $idlivreur=$commande->getidlivreur();
+        $etatcom=$commande->getetatcom();
+
+
+
+$datas = array( ':idc'=>$idC, ':nomC'=>$nomC,':famille'=>$famille);
+	
+		$req->bindValue(':idC',$idC);
+		$req->bindValue(':nomC',$nomC);
+		$req->bindValue(':famille',$famille);
+            $s=$req->execute();
+			
+           // header('Location: index.php');
+        }
+        catch (Exception $e){
+            echo " Erreur ! ".$e->getMessage();
+   echo " Les datas : " ;
+  print_r($datas);
+        }
+		
+	}
+
+}
 ?>
